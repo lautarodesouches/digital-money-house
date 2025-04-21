69f0e07eea18654fa6be1c6254929f6dfbb82cfa
lautarodesouches <lautarodesouches@outlook.com>
2025-04-21 17:20:46 -0300

    [Feat] Login suspense

--- app/iniciar-sesion/page.tsx
+++ app/iniciar-sesion/page.tsx
@@ -1,94 +1,10 @@
-'use client'
-import { ButtonPrimary, ButtonSecondary, Footer, Header } from '@/components'
-import styles from './page.module.css'
-import Link from 'next/link'
-import { ROUTES } from '../../routes'
-import { useActionState, useEffect, useState } from 'react'
-import { login } from './actions'
-import { useSearchParams } from 'next/navigation'
-
-interface Content {
-    stage: 'email' | 'password'
-    title: string
-    buttonText: string
-}
+import { Suspense } from 'react'
+import Content from './content'
 
 export default function Login() {
-    const searchParams = useSearchParams()
-    const exito = searchParams.has('exito')
-
-    const [state, formAction] = useActionState(login, null)
-    const [isLoading, setIsLoading] = useState(false)
-    const [content, setContent] = useState<Content>({
-        stage: 'email',
-        title: '¡Hola! Ingresá tu e-mail',
-        buttonText: 'Continuar',
-    })
-
-    useEffect(() => {
-        setIsLoading(false)
-
-        if (
-            state?.error === 'Contraseña incorrecta. Vuelve a intentarlo.' &&
-            content.stage === 'email'
-        ) {
-            state.error = ''
-            setContent({
-                stage: 'password',
-                title: 'Ingresá tu contraseña',
-                buttonText: 'Ingresar',
-            })
-        }
-    }, [state, content.stage])
-
     return (
-        <>
-            <Header background="secondary" />
-            <main className={styles.main}>
-                <form
-                    className={styles.form}
-                    action={formAction}
-                    onSubmit={() => setIsLoading(true)}
-                >
-                    <h2 className={styles.form__h2}>{content.title}</h2>
-                    <input
-                        className={styles.form__input}
-                        name="email"
-                        type={content.stage === 'email' ? 'text' : 'hidden'}
-                        placeholder="Correo electrónico"
-                        required
-                    />
-                    <input
-                        className={styles.form__input}
-                        name="password"
-                        type={
-                            content.stage === 'password' ? 'password' : 'hidden'
-                        }
-                        placeholder="Contraseña"
-                        required
-                    />
-                    <div className={styles.form__div}>
-                        <ButtonPrimary disabled={isLoading}>
-                            {content.buttonText}
-                            {isLoading && (
-                                <span className={styles.progress}></span>
-                            )}
-                        </ButtonPrimary>
-                    </div>
-                    {content.stage === 'email' && !exito && (
-                        <Link
-                            className={styles.form__link}
-                            href={ROUTES.CREAR_CUENTA}
-                        >
-                            <ButtonSecondary>Crear cuenta</ButtonSecondary>
-                        </Link>
-                    )}
-                    <span className={styles.form__error}>
-                        {state?.error && state.error}
-                    </span>
-                </form>
-            </main>
-            <Footer />
-        </>
+        <Suspense fallback={<div>Cargando login...</div>}>
+            <Content />
+        </Suspense>
     )
 }
