49370a6ccb3fd84227230256a4096aacb912603a
lautarodesouches <lautarodesouches@outlook.com>
2025-05-07 11:25:38 -0300

    [Feat] Profile update name in menu

--- app/dashboard/perfil/profileInfo.tsx
+++ app/dashboard/perfil/profileInfo.tsx
@@ -4,6 +4,7 @@ import styles from './page.module.css'
 import { useEffect, useState } from 'react'
 import { API_URL } from '@/constants'
 import { EditIcon } from '@/components/Icons'
+import { useRouter } from 'next/navigation'
 
 interface Props {
     token: string
@@ -12,20 +13,22 @@ interface Props {
 }
 
 export default function ProfileInfo({ token, userId, user }: Props) {
+    const router = useRouter()
+
     const [localUser, setLocalUser] = useState<UserType>(user)
 
     const [loadingField, setLoadingField] = useState<string | null>(null)
 
     const sendChanges = async (updatedUser: UserType) => {
-        const name = updatedUser.firstname.split(' ')
+        const { dni, email, firstname, lastname, password, phone } = updatedUser
 
         const body = JSON.stringify({
-            dni: updatedUser.dni,
-            email: updatedUser.email,
-            firstname: name[0],
-            lastname: name[1],
-            password: updatedUser.password,
-            phone: updatedUser.phone,
+            dni,
+            email,
+            firstname,
+            lastname,
+            password,
+            phone,
         })
 
         try {
@@ -42,6 +45,7 @@ export default function ProfileInfo({ token, userId, user }: Props) {
         } catch (err) {
             console.error(err)
         } finally {
+            router.refresh()
             setLoadingField(null)
         }
     }
@@ -51,9 +55,21 @@ export default function ProfileInfo({ token, userId, user }: Props) {
 
         setLoadingField(name)
 
-        const updatedUser = {
-            ...localUser,
-            [name]: value,
+        let updatedUser = { ...localUser }
+
+        if (name === 'name') {
+            const [firstname, ...rest] = value.trim().split(' ')
+            const lastname = rest.join(' ')
+            updatedUser = {
+                ...updatedUser,
+                firstname,
+                lastname,
+            }
+        } else {
+            updatedUser = {
+                ...updatedUser,
+                [name]: value,
+            }
         }
 
         setLocalUser(updatedUser)
@@ -77,17 +93,15 @@ export default function ProfileInfo({ token, userId, user }: Props) {
                     readOnly: true,
                 },
                 {
-                    id: 'firstname',
+                    id: 'name',
                     label: 'Nombre y apellido',
                     type: 'text',
-                    hasIcon: true,
                     value: `${localUser.firstname} ${localUser.lastname}`,
                 },
                 {
                     id: 'dni',
                     label: 'DNI',
                     type: 'number',
-                    hasIcon: true,
                     value: localUser.dni,
                     readOnly: true,
                 },
@@ -95,17 +109,15 @@ export default function ProfileInfo({ token, userId, user }: Props) {
                     id: 'phone',
                     label: 'Teléfono',
                     type: 'tel',
-                    hasIcon: true,
                     value: localUser.phone,
                 },
                 {
                     id: 'password',
                     label: 'Contraseña',
                     type: 'password',
-                    hasIcon: true,
                     value: '1234567890',
                 },
-            ].map(({ id, label, type, hasIcon, value, readOnly }) => (
+            ].map(({ id, label, type, value, readOnly }) => (
                 <div key={id} className={styles.info__group}>
                     <label className={styles.info__label} htmlFor={id}>
                         {label}
@@ -120,7 +132,7 @@ export default function ProfileInfo({ token, userId, user }: Props) {
                         disabled={loadingField === id}
                         readOnly={readOnly}
                     />
-                    {hasIcon && <EditIcon styles={styles.info__svg} />}
+                    {!readOnly && <EditIcon styles={styles.info__svg} />}
                 </div>
             ))}
         </section>
